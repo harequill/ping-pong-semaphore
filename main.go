0326de0cf920d552670db492896a5585f74b2327
Harequill <harequill@proton.me>
2025-12-10 12:08:59 -0300

    now it pings and it pongs

--- main.go
+++ main.go
@@ -5,25 +5,34 @@ import (
 	"time"
 )
 
-func ping(out chan<- string) {
+func ping(out chan<- string, pingTurn <-chan struct{}, pongTurn chan<- struct{}) {
+	//struct for semaphores is the convention in go, cause takes 0 bytes.
 	for {
+		<-pingTurn
 		out <- "ping"
 		time.Sleep(time.Second * 2)
+		pongTurn <- struct{}{}
 	}
 }
 
-func pong(out chan<- string) {
+func pong(out chan<- string, pongTurn <-chan struct{}, pingTurn chan<- struct{}) {
 	for {
+		<-pongTurn
 		out <- "pong"
 		time.Sleep(time.Second * 2)
+		pingTurn <- struct{}{}
 	}
 }
 
 func main() {
 	out := make(chan string)
+	pingTurn := make(chan struct{})
+	pongTurn := make(chan struct{})
 
-	go ping(out)
-	go pong(out)
+	go ping(out, pingTurn, pongTurn)
+	go pong(out, pongTurn, pingTurn)
+
+	pingTurn <- struct{}{} //starts the semaphore with "ping"
 
 	for {
 		fmt.Println(<-out)
