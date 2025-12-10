1808a14a25becf38902bf0c9d9d54cb9c3c8c7ce
Harequill <harequill@proton.me>
2025-12-10 12:01:19 -0300

    ping, 1, 2, ping, 1, 2 ping

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func ping(out chan<- string) {
+	for {
+		out <- "ping"
+		time.Sleep(time.Second * 2)
+	}
+}
 
 func main() {
-	fmt.Println("Harello")
+	out := make(chan string)
+
+	go ping(out)
+
+	for {
+		fmt.Println(<-out)
+	}
+
 }
